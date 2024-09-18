95990ef8fd95e281232113fd021c540f6c55481b
alfuwu <130256600+alfuwu@users.noreply.github.com>
2024-09-17 23:06:48 -0700

    1.9 upd

--- DayCounterMod.cs
+++ DayCounterMod.cs
@@ -1,6 +1,4 @@
-using Humanizer;
 using Microsoft.Xna.Framework;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using Terraria;
@@ -11,10 +9,8 @@ using Terraria.ModLoader.Config;
 using Terraria.ModLoader.IO;
 using Terraria.UI;
 
-namespace DayCounterMod
-{
-	public class DayCounterMod : Mod
-    {
+namespace DayCounterMod {
+	public class DayCounterMod : Mod {
         internal DayUIText text;
         public UserInterface textInterface;
 
@@ -22,45 +18,37 @@ namespace DayCounterMod
 
         internal static DayCounterModSystem system;
 
-        public override void Load()
-        {
+        public override void Load() {
             instance = this;
 
-            if (!Main.dedServ)
-            {
-                text = new DayUIText();
+            if (!Main.dedServ) {
+                text = new();
                 text.Initialize();
-                textInterface = new UserInterface();
+                textInterface = new();
                 textInterface.SetState(text);
             }
         }
 
-        public void UpdateUI(GameTime gameTime)
-        {
+        public void UpdateUI(GameTime gameTime) {
             // it will only draw if the player is not on the main menu
             if (!Main.gameMenu)
                 textInterface?.Update(gameTime);
         }
 
-        public void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-        {
+        public void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) {
             int inventoryLayerIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text")) - 8;
             layers.Insert(inventoryLayerIndex, new LegacyGameInterfaceLayer("Day Counter: UI", DrawDayCounterUI, InterfaceScaleType.UI));
         }
 
-        private bool DrawDayCounterUI()
-        {
+        private bool DrawDayCounterUI() {
             // it will only draw if the player is not on the main menu
             if (!Main.gameMenu)
-            {
                 if (Main.LocalPlayer?.GetModPlayer<DayCounterPlayer>().showUI ?? false) // check all three booleans that handle drawing the status bars, default to false if it can't access the showUI boolean
                     textInterface.Draw(Main.spriteBatch, new GameTime());
-            }
             return true;
         }
 
-        public class DayCounterModSystem : ModSystem
-        {
+        public class DayCounterModSystem : ModSystem {
             public ModKeybind ShowUI { get; private set; }
             public ModKeybind SwitchUIState { get; private set; } // switches between total days in world, and total amount of days the player has been thru (player days is saved to the player's .tplr file, which means that it persists across all worlds)
 
@@ -73,58 +61,42 @@ namespace DayCounterMod
 
             public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) => instance.ModifyInterfaceLayers(layers);
 
-            public override void Load()
-            {
-                base.Load();
+            public override void Load() {
                 system = this; // this is the modsystem
                 ShowUI = KeybindLoader.RegisterKeybind(Mod, "ShowUI", "Period");
                 SwitchUIState = KeybindLoader.RegisterKeybind(Mod, "SwitchUIState", "X");
             }
 
-            public override void Unload()
-            {
-                base.Unload();
+            public override void Unload() {
                 ShowUI = null;
                 SwitchUIState = null;
             }
 
-            public override void OnWorldLoad()
-            {
-                base.OnWorldLoad();
+            public override void OnWorldLoad() {
                 tru = Main.dayTime;
                 canUpdate = true;
             }
 
-            public override void OnWorldUnload()
-            {
+            public override void OnWorldUnload() {
                 days = 0;
                 nights = 0;
 
                 canUpdate = false;
-                base.OnWorldUnload();
                 tru = true;
             }
 
-            public override void PostUpdateTime()
-            {
-                base.PostUpdateTime();
-                if (tru && !Main.dayTime && canUpdate)
-                {
+            public override void PostUpdateTime() {
+                if (tru && !Main.dayTime && canUpdate) {
                     tru = false;
                     nights++;
-                }
-                else if (!tru && Main.dayTime && canUpdate)
-                {
+                } else if (!tru && Main.dayTime && canUpdate) {
                     tru = true;
                     days++;
                 }
             }
 
             #region[Saving]
-            public override void SaveWorldData(TagCompound tag)
-            {
-                base.SaveWorldData(tag);
-
+            public override void SaveWorldData(TagCompound tag) {
                 #region[Saving Values]
                 tag.Add("days", days);
                 tag.Add("nights", nights);
@@ -132,13 +104,10 @@ namespace DayCounterMod
                 #endregion
             }
 
-            public override void LoadWorldData(TagCompound tag)
-            {
+            public override void LoadWorldData(TagCompound tag) {
                 canUpdate = false;
                 days = 0;
                 nights = 0;
-
-                base.LoadWorldData(tag);
 
                 #region[Setting Values]
                 if (tag.TryGet("days", out int sDays))
@@ -154,41 +123,56 @@ namespace DayCounterMod
         }
     }
 
-    public class Client : ModConfig
-    {
+    public class Client : ModConfig {
         public override ConfigScope Mode => ConfigScope.ClientSide; // only affects how the day counter appears on the client
 
         [Header("Visual")]
         
-        [Label("Show Icon")]
         [DefaultValue(true)]
         [ReloadRequired]
         public bool ShouldShowIcon = true;
 
-        [Label("Day Counter Visible By Default")]
         [DefaultValue(true)]
         [ReloadRequired]
         public bool DayCounterVisibleByDefault = true;
 
+        [DefaultValue("Bottom Left")]
+        [OptionStrings(["Bottom Left", "Left", "Top Left", "Bottom Right", "Right", "Top Right", "Bottom", "Center", "Top"])]
+        [ReloadRequired]
+        [DrawTicks]
+        public string AnchorLocation = "Bottom Left";
+
+        [DefaultValue(typeof(Vector2), "0.5, 0.5")]
+        [ReloadRequired]
+        public Vector2 AnchorOffset = new(0.5f, 0.5f);
+
         [Header("Advanced")]
 
-        [Label("World-oriented Text")]
         [DefaultValue("Days: {WORLD_DAYS}")]
         public string ServerDays = "Days: {WORLD_DAYS}";
 
-        [Label("Player-oriented Text")]
         [DefaultValue("Days: {PLAYER_DAYS}")]
         public string PlayerDays = "Days: {PLAYER_DAYS}";
 
-        [Label("Default Type")]
         [DefaultValue(1)]
         [ReloadRequired]
         [Range(1, 2)]
         public int DefaultType = 1;
+
+        public string GetServerDays(int worldDays = 0, int worldNights = 0, int playerDays = 0, int playerNights = 0) => ServerDays
+            .Replace("{WORLD_DAYS}", worldDays.ToString())
+            .Replace("{WORLD_NIGHTS}", worldNights.ToString())
+            .Replace("{PLAYER_DAYS}", playerDays.ToString())
+            .Replace("{PLAYER_NIGHTS}", playerNights.ToString());
+
+        public string GetPlayerDays(int worldDays = 0, int worldNights = 0, int playerDays = 0, int playerNights = 0) => PlayerDays
+            .Replace("{WORLD_DAYS}", worldDays.ToString())
+            .Replace("{WORLD_NIGHTS}", worldNights.ToString())
+            .Replace("{PLAYER_DAYS}", playerDays.ToString())
+            .Replace("{PLAYER_NIGHTS}", playerNights.ToString());
     }
 
-    public class DayCounterPlayer : ModPlayer // for days that this player has been on a specific world
-    {
+    public class DayCounterPlayer : ModPlayer { // for days that this player has been on a specific world
         public int days = 0;
         public int nights = 0;
         public bool tru = true;
@@ -198,86 +182,56 @@ namespace DayCounterMod
         public int temp = -1;
         public bool? tempTru = null;
 
-        public override void OnEnterWorld()
-        {
-            base.OnEnterWorld();
-
+        public override void OnEnterWorld() {
             tru = Main.dayTime; // set current time
             canUpdate = true;
         }
 
-        public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
-        {
+        public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource) {
             temp = days;
             tempTru = tru;
-            return base.PreKill(damage, hitDirection, pvp, ref playSound, ref genGore, ref damageSource);
+            return true;
         }
 
-        public override void OnRespawn()
-        {
-            base.OnRespawn();
-
+        public override void OnRespawn() {
             if (tempTru == tru && temp != -1 && temp != days) // if this player is the one who respawns
-            {
                 days = temp;
-            }
         }
 
-        public override void Load()
-        {
-            base.Load();
+        public override void Load() {
             showUI = ModContent.GetInstance<Client>().DayCounterVisibleByDefault;
             type = ModContent.GetInstance<Client>().DefaultType != 1;
         }
 
-        public override void Unload()
-        {
+        public override void Unload() {
             days = 0;
             nights = 0;
 
             canUpdate = false;
-            base.Unload();
             tru = true;
         }
 
-        public override void PostUpdate()
-        {
-            base.PostUpdate();
-            if (tru && !Main.dayTime && canUpdate)
-            {
+        public override void PostUpdate() {
+            if (tru && !Main.dayTime && canUpdate) {
                 tru = false;
                 nights++;
-            }
-            else if (!tru && Main.dayTime && canUpdate)
-            {
+            } else if (!tru && Main.dayTime && canUpdate) {
                 tru = true;
                 days++;
             }
         }
 
-        public override void ProcessTriggers(TriggersSet triggersSet) // keybinds
-        {
-            base.ProcessTriggers(triggersSet);
-            DayCounterMod myMod = Mod as DayCounterMod;
-
-            if (myMod != null && DayCounterMod.system != null)
-            {
+        public override void ProcessTriggers(TriggersSet triggersSet) { // keybinds
+            if (Mod as DayCounterMod != null && DayCounterMod.system != null) {
                 if (DayCounterMod.system?.ShowUI?.JustPressed == true)
-                {
                     showUI = !showUI; // toggle UI
-                }
                 if (DayCounterMod.system?.SwitchUIState?.JustPressed == true)
-                {
                     type = !type; // switch type
-                }
             }
         }
 
         #region[Saving]
-        public override void SaveData(TagCompound tag)
-        {
-            base.SaveData(tag);
-
+        public override void SaveData(TagCompound tag) {
             #region[Saving Values]
             tag.Add("days", days);
             tag.Add("nights", nights);
@@ -285,13 +239,10 @@ namespace DayCounterMod
             #endregion
         }
 
-        public override void LoadData(TagCompound tag)
-        {
+        public override void LoadData(TagCompound tag) {
             canUpdate = false;
             days = 0;
             nights = 0;
-
-            base.LoadData(tag);
 
             #region[Setting Values]
             if (tag.TryGet("days", out int sDays))
--- UI.cs
+++ UI.cs
@@ -4,39 +4,37 @@ using Terraria.GameContent.UI.Elements;
 using Terraria.ModLoader;
 using Terraria.UI;
 
-namespace DayCounterMod
-{
-    public class DayUIText : UIState
-    {
+namespace DayCounterMod {
+    public class DayUIText : UIState {
         public UIElement area;
         public UIImage icon;
         public UIText text;
 
-        public override void OnInitialize()
-        {
-            area = new UIElement();
+        public override void OnInitialize() {
+            Client client = ModContent.GetInstance<Client>();
+            area = new();
+            AdjustLocation(client.AnchorLocation);
             area.Width.Set(182, 0);
             area.Height.Set(60, 0);
-            area.HAlign = 0;
-            area.VAlign = 1;
-            area.Top.Set(-7, 0);
-            area.Left.Set(10, 0);
+            area.Top.Set(-7, client.AnchorOffset.Y - 0.5f);
+            area.Left.Set(10, client.AnchorOffset.X - 0.5f);
 
-            text = new UIText(ModContent.GetInstance<Client>().ServerDays.Replace("{WORLD_DAYS}", "0").Replace("{WORLD_NIGHTS}", "0").Replace("{PLAYER_DAYS}", "0").Replace("{PLAYER_NIGHTS}", "0"), (float) 0.8);
-            text.HAlign = 0;
+            text = new(client.GetServerDays(), 0.8f) {
+                HAlign = 0,
+                TextOriginX = 0
+            };
             text.Width.Set(138, 0);
             text.Height.Set(34, 0);
             text.Top.Set(40, 0);
             text.Left.Set(48, 0);
-            text.TextOriginX = 0;
 
-            if (ModContent.GetInstance<Client>().ShouldShowIcon)
-            {
-                icon = new UIImage(ModContent.Request<Texture2D>("DayCounterMod/sun"));
+            if (client.ShouldShowIcon) {
+                icon = new(ModContent.Request<Texture2D>("DayCounterMod/sun")) {
+                    VAlign = 1
+                };
                 icon.Width.Set(32, 0);
                 icon.Height.Set(32, 0);
                 icon.Left.Set(12, 0);
-                icon.VAlign = 1;
                 area.Append(icon);
             }
 
@@ -44,10 +42,52 @@ namespace DayCounterMod
             Append(area);
         }
 
-        protected override void DrawSelf(SpriteBatch spriteBatch)
-        {
-            base.DrawSelf(spriteBatch);
-            string currentText = Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().type == false ? ModContent.GetInstance<Client>().ServerDays.Replace("{WORLD_DAYS}", DayCounterMod.system.days.ToString()).Replace("{WORLD_NIGHTS}", DayCounterMod.system.nights.ToString()).Replace("{PLAYER_DAYS}", Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().days.ToString()).Replace("{PLAYER_NIGHTS}", Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().nights.ToString()) : ModContent.GetInstance<Client>().PlayerDays.Replace("{WORLD_DAYS}", DayCounterMod.system.days.ToString()).Replace("{WORLD_NIGHTS}", DayCounterMod.system.nights.ToString()).Replace("{PLAYER_DAYS}", Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().days.ToString()).Replace("{PLAYER_NIGHTS}", Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().nights.ToString());
+        public void AdjustLocation(string anchor) {
+            switch (anchor) {
+                default:
+                case "Bottom Left":
+                    area.HAlign = 0.0f;
+                    area.VAlign = 1.0f;
+                    break;
+                case "Left":
+                    area.HAlign = 0.0f;
+                    area.VAlign = 0.5f;
+                    break;
+                case "Top Left":
+                    area.HAlign = 0.0f;
+                    area.VAlign = 0.1f;
+                    break;
+                case "Bottom Right":
+                    area.HAlign = 1.0f;
+                    area.VAlign = 1.0f;
+                    break;
+                case "Right":
+                    area.HAlign = 1.0f;
+                    area.VAlign = 0.5f;
+                    break;
+                case "Top Right":
+                    area.HAlign = 1.0f;
+                    area.VAlign = 0.0f;
+                    break;
+                case "Bottom":
+                    area.HAlign = 0.5f;
+                    area.VAlign = 1.0f;
+                    break;
+                case "Center": // why would you ever use this
+                    area.HAlign = 0.5f;
+                    area.VAlign = 0.5f;
+                    break;
+                case "Top":
+                    area.HAlign = 0.5f;
+                    area.VAlign = 0.0f;
+                    break;
+            }
+        }
+
+        protected override void DrawSelf(SpriteBatch spriteBatch) {
+            string currentText = Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().type ?
+                ModContent.GetInstance<Client>().GetPlayerDays(DayCounterMod.system.days, DayCounterMod.system.nights, Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().days, Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().nights) :
+                ModContent.GetInstance<Client>().GetServerDays(DayCounterMod.system.days, DayCounterMod.system.nights, Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().days, Main.LocalPlayer.GetModPlayer<DayCounterPlayer>().nights);
             text.SetText(currentText);
         }
     }
